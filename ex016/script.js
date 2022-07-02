6d4172f05e32d955c86db45e25f578f68b39f3f3
Diego de Araujo <101844511+Daraujocamara@users.noreply.github.com>
2022-07-01 22:26:15 -0300

    finishid ex014

--- ex016/script.js
+++ ex016/script.js
@@ -5,7 +5,26 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[ERRO] Falta dados!')
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO] Faltam dados!')
     }else {
-        res.innerHTML=('Contando...')
+        res.innerHTML = 'contando: <br>'  
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if(p ==0){
+            window.alert('passo inválido! considerando PASSO 1')
+            p = 1
+        }
+        if (i < f ){
+         for(let c = i; c <= f; c += p ){
+            res.innerHTML += ` ${c}\u{1f449}`   
+            }        
+        }else{
+            for( let c = i; c >= f; c-= p ){
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
 }
